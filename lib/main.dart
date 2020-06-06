5f21563c89e0924cda6e681a7aca577475021bd7
jaymes15 <jamesoluwatomisinomtosho@gmail.com>
2020-06-05 18:35:16 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner:false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+
+class HomePage  extends StatefulWidget{
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState  extends State<HomePage>{
+  int firstnum;
+  int secondnum;
+  String texttodisplay ="";
+  String res ;
+  String operations;
+
+  void btnclicked(String btntext){
+    if(btntext == "C"){
+      texttodisplay ="";
+      firstnum = 0;
+      secondnum = 0;
+      res = "";
+    }
+    else if(btntext == "+" || btntext =="x" || btntext == "-" || btntext =="/"){
+      firstnum = int.parse(texttodisplay);
+      res = "";
+      operations = btntext;
+    }
+    else if(btntext == "="){
+      secondnum = int.parse(texttodisplay);
+      if(operations == "+"){
+        res = (firstnum + secondnum).toString();
+      }
+      if(operations == "-"){
+        res = (firstnum - secondnum).toString();
+      }
+      if(operations == "/"){
+        res = (firstnum ~/ secondnum).toString();
+      }
+      if(operations == "x"){
+        res = (firstnum * secondnum).toString();
+      }
+    }
+    else{
+      res = (texttodisplay + btntext).toString();
+    }
+ setState(() {
+   texttodisplay = res;
+ });
+  }
+
+  Widget custombutton(String btnval){
+    return Expanded(
+      child: OutlineButton(
+        splashColor: Colors.blue,
+        padding:EdgeInsets.all(25.0),
+        onPressed: () => btnclicked(btnval),
+        child: Text(
+          btnval,
+          style: TextStyle(
+            fontSize: 25.0,
+          ),
+        ),
+      ),
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context){
+
+    return Scaffold(
+
+      appBar: AppBar(
+        title: Text(
+          "Calculator",
+        ),
+      ),
+
+      body: Container(
+
+        child:Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(10.0),
+                alignment: Alignment.bottomRight,
+                child: Text(""
+                    "$texttodisplay",
+                style: TextStyle(
+                  fontSize: 70.0,
+                  fontWeight: FontWeight.w600,
+                  fontFamily:"Roboto",
+                ),
+                )
+              ),
+            ),
+            Row(
+              children: <Widget>[
+                custombutton("9"),
+                custombutton("8"),
+                custombutton("7"),
+                custombutton("+"),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                custombutton("6"),
+                custombutton("5"),
+                custombutton("4"),
+                custombutton("-"),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                custombutton("3"),
+                custombutton("2"),
+                custombutton("1"),
+                custombutton("x"),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                custombutton("C"),
+                custombutton("0"),
+                custombutton("="),
+                custombutton("/"),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+}
+
